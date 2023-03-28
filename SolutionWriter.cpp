cca04e9938bb2c1b1393c73ea82b725af5080734
maciej.filanowicz <mfilon13@gmail.com>
2023-03-28 18:56:01 +0200

    fix summary

--- SolutionWriter.cpp
+++ SolutionWriter.cpp
@@ -8,8 +8,8 @@ SolutionWriter::SolutionWriter(Instance* instance, const string& dest_dir, strin
     this->dest_dir = dest_dir;
     this->destination_filename = dest_dir +algorithm_name+"_"+ instance->name + ".sol";
     this->number_of_runs = number_of_runs;
-    this->calculated_solutions = new int* [this->instance->size];
-    for (int i = 0; i < this->instance->size; i++) this->calculated_solutions [i] = new int[number_of_runs];
+    this->calculated_solutions = new int* [this->instance->get_size()];
+    for (int i = 0; i < this->instance->get_size(); i++) this->calculated_solutions [i] = new int[number_of_runs];
     this->time_table = new long long[number_of_runs];
     this->optimal_solution = get_optimal_solution(dest_dir+instance->name+".opt.tour");
 }
@@ -43,6 +43,7 @@ int* SolutionWriter::get_optimal_solution(string optimal_solution_path)
 }
 void SolutionWriter::append_solution(int* solution, long long elapsed_time){
     for (int i = 0; i < this->instance->get_size(); i++) {
+
         this->calculated_solutions[i][this->number_of_appended_solutions] = solution[i];
     }
     this->time_table[this->number_of_appended_solutions] = elapsed_time;
@@ -90,8 +91,13 @@ void SolutionWriter::print_summary()
     int optimal_cost = Solver::cost(this->optimal_solution, this->instance->get_matrix(), this->instance->get_size());
     double avg_calculated_cost = 0.0;
     double avg_elapsed_time = 0.0;
+    int* solution = new int[this->instance->get_size()];
     for (int i = 0; i < this->number_of_appended_solutions; i++) {
-        avg_calculated_cost += Solver::cost(this->calculated_solutions[i], this->instance->get_matrix(),this->instance->get_size());
+        for (int j = 0; j < instance->get_size(); j++) {
+            solution[j] = this->calculated_solutions[j][i];
+
+        }
+        avg_calculated_cost += Solver::cost(solution, this->instance->get_matrix(),this->instance->get_size());
         avg_elapsed_time+= this->time_table[i];
     }
     avg_calculated_cost /= this->number_of_appended_solutions;
