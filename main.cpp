147442f8918c8c694c7511cec8c7d6a074de0742
Mikołaj Kruś <mikolaj.krus@student.put.poznan.pl>
2023-03-29 21:35:53 +0200

    iterations counter

--- main.cpp
+++ main.cpp
@@ -23,21 +23,27 @@ int main() {
             "st70.tsp"
     };
     int number_of_runs = 10;
-    string data_dir = "/Users/maciej.filanowicz/tsp-local-search/data/";
+//    string data_dir = "/Users/maciej.filanowicz/tsp-local-search/data/";
+    string data_dir = "/home/mikikrus/CLionProjects/tsp-local-search/data/";
 
     for (auto & instance_name : instance_names) {
         cout << "Instance: " << instance_name << endl;
         auto *instance = new Instance(data_dir + std::string(instance_name), true);
         auto *solution_writer = new SolutionWriter(instance, data_dir,"greedy", number_of_runs);
         for (int j =0; j < number_of_runs; j++) {
+            int iterations;
+            int* solution;
             auto startTime = std::chrono::steady_clock::now();
-            int* solution = Solver::greedy(instance,solution_writer);
+            std::tie(solution, iterations) = Solver::greedy(instance,solution_writer);
             auto endTime = std::chrono::steady_clock::now();
             auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds >(endTime - startTime).count();
             solution_writer->append_solution(solution,elapsedTime,"final_order", false);
         }
         solution_writer->print_summary();
         solution_writer->write_solution();
+//        int* y = Solver::steepest(instance, solution_writer);
+//        return 0;
     }
+
     return 0;
 }
